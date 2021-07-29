b1d2f1e6ee0f60b31b09b96cc349b281382366e6
rnakai <rnakai@student.42tokyo.jp>
2021-07-29 12:59:16 +0900

    テンプレートからヘッダと定義を自動生成できるようにした

--- utils.sh
+++ utils.sh
@@ -1,8 +1,18 @@
 create_file() {
     for class_file_name in "${@}" ;
     do
-        touch ${class_file_name}.cpp ${class_file_name}.hpp
+        # create cpp file
+        sed -e "s/__CLASS_NAME__/${class_file_name}/g" \
+            ${COPLIEN_LIB_PATH}/template/template.cpp > ${class_file_name}.cpp
         printf "${GREEN}create ${class_file_name}.cpp${RESET}\n"
+
+        # create hpp file
+        sed -e "s/__CLASS_NAME__/${class_file_name}/g" \
+            ${COPLIEN_LIB_PATH}/template/template.hpp > ${class_file_name}.hpp
+
+        HEADER_CONST="`echo ${class_file_name} | tr [a-z] [A-Z]`"
+        HEADER_CONST="`echo ${HEADER_CONST} | tr . _`"
+        sed -i -e "s/__HEADER__/${HEADER_CONST}_HPP/g" ${class_file_name}.hpp
         printf "${GREEN}create ${class_file_name}.hpp${RESET}\n"
     done
 }
