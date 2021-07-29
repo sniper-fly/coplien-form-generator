0f1ee86d1e1a64e927723d005e5071fc7b8d2fd7
rnakai <rnakai@student.42tokyo.jp>
2021-07-29 13:16:53 +0900

    create file関数のsed iオプションを排斥してGNU以外でも使えるようにした

--- utils.sh
+++ utils.sh
@@ -7,12 +7,12 @@ create_file() {
         printf "${GREEN}create ${class_file_name}.cpp${RESET}\n"
 
         # create hpp file
-        sed -e "s/__CLASS_NAME__/${class_file_name}/g" \
-            ${COPLIEN_LIB_PATH}/template/template.hpp > ${class_file_name}.hpp
-
+        class_name_replaced="`sed -e s/__CLASS_NAME__/${class_file_name}/g \
+            ${COPLIEN_LIB_PATH}/template/template.hpp`"
         HEADER_CONST="`echo ${class_file_name} | tr [a-z] [A-Z]`"
         HEADER_CONST="`echo ${HEADER_CONST} | tr . _`"
-        sed -i -e "s/__HEADER__/${HEADER_CONST}_HPP/g" ${class_file_name}.hpp
+        echo "${class_name_replaced}" |
+        sed -e "s/__HEADER__/${HEADER_CONST}_HPP/g" > ${class_file_name}.hpp
         printf "${GREEN}create ${class_file_name}.hpp${RESET}\n"
     done
 }
