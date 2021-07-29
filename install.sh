422181faf85d84ba831d2c6951d012147905e50a
rnakai <rnakai@student.42tokyo.jp>
2021-07-29 14:48:10 +0900

    COPLIEN_LIB_PATHとCMD_NAMEを検索して、より安全にインストールできるようにした

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 INSTALL_PATH="${HOME}/.local/bin"
-CMD_NAME="coplien"
+CMD_NAME="cpform"
 
 ln -sf "`pwd`/coplien.sh" "${INSTALL_PATH}/${CMD_NAME}"
 
-sed -i -e "3s!.*!COPLIEN_LIB_PATH='`pwd`' ## Do not delete the empty line above!" coplien.sh
-sed -i -e "4s!.*!CMD_NAME='${CMD_NAME}'!" coplien.sh
+sed -i -e "s@^COPLIEN_LIB_PATH=.*@COPLIEN_LIB_PATH='`pwd`'@" coplien.sh
+sed -i -e "s@^CMD_NAME=.*@CMD_NAME='${CMD_NAME}'@" coplien.sh
+
+#TODO: iオプションを置き換える。シェル変数自体は用意しておき、文末を置換する形で設定を変える
+# create コマンドの条件分岐
